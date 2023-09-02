c7168fb687e1790935e2cfafa5dfd17238f68adb
Faeze <faezeahmadi7173@gmail.com>
2023-09-02 21:54:46 +0330

    feat/faeze/js-ts-learning

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let age = 12;
+let nam = "faeze";
+const person = ["ali", "faeze"];
+person.push("zahra");
+console.log(person);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,7 @@
+let age: number = 12;
+let nam: string = "faeze";
+type person = string;
+const person: person[] = ["ali", "faeze"];
+person.push("zahra")
+
+console.log(person)
