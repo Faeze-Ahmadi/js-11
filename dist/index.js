a708534ac93d2a883c60530f9ba8a7a66d2d2f24
Faeze <faezeahmadi7173@gmail.com>
2023-09-03 01:08:26 +0330

    feat/faeze/js-9

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,12 @@
 "use strict";
-let age = 12;
-let nam = "faeze";
-const person = ["ali", "faeze"];
-person.push("zahra");
-console.log(person);
+// let age: number = 12;
+// let nam: string = "faeze";
+// type person = string;
+// const person: person[] = ["ali", "faeze"];
+// person.push("zahra")
+const contactList = [];
 const email = document.querySelector("#email");
+const password = document.querySelector("#password");
 document.addEventListener("click", () => {
     console.log("ml");
 });
@@ -13,7 +15,15 @@ email === null || email === void 0 ? void 0 : email.addEventListener("click", ()
 });
 const button = document.querySelector("#button");
 button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
-    console.log(email.value);
+    var _a, _b;
+    console.log(email === null || email === void 0 ? void 0 : email.value);
+    const newContact = {
+        id: crypto.randomUUID(),
+        contactName: (_a = email === null || email === void 0 ? void 0 : email.value) !== null && _a !== void 0 ? _a : '',
+        contactNumber: (_b = password === null || password === void 0 ? void 0 : password.value) !== null && _b !== void 0 ? _b : '',
+        storage: "SIM",
+        avatar: null,
+    };
 });
 console.log(email);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,13 +1,27 @@
-let age: number = 12;
-let nam: string = "faeze";
-type person = string;
-const person: person[] = ["ali", "faeze"];
-person.push("zahra")
+// let age: number = 12;
+// let nam: string = "faeze";
+// type person = string;
+// const person: person[] = ["ali", "faeze"];
+// person.push("zahra")
 
-console.log(person)
+// console.log(person)
+
+type contactProps = {
+    id: string;
+    contactName: string;
+    contactNumber: string | Number;
+    storage: "SIM" | "Device";
+    avatar: string | null;
+
+}
+
+type contactListType = contactProps[];
+const contactList: contactListType = [];
 
 
 const email = document.querySelector<HTMLInputElement>("#email");
+const password = document.querySelector<HTMLInputElement>("#password");
+
 document.addEventListener("click", () => {
     console.log("ml")
 });
@@ -19,6 +33,13 @@ email?.addEventListener("click", () => {
 const button = document.querySelector<HTMLButtonElement>("#button");
 button?.addEventListener("click", () => {
     console.log(email?.value)
+    const newContact: contactProps = {
+        id: crypto.randomUUID(),
+        contactName: email?.value ?? '',
+        contactNumber: password?.value ?? '',
+        storage: "SIM",
+        avatar: null,
+    }
 });
 
 
