a638546eaa5e41fa0803053923ba3172bbee5631
Faeze <faezeahmadi7173@gmail.com>
2023-09-03 00:05:57 +0330

    feat/faeze/js-08

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,16 @@ let nam = "faeze";
 const person = ["ali", "faeze"];
 person.push("zahra");
 console.log(person);
+const email = document.querySelector("#email");
+document.addEventListener("click", () => {
+    console.log("ml");
+});
+email === null || email === void 0 ? void 0 : email.addEventListener("click", () => {
+    console.log("mjkk");
+});
+const button = document.querySelector("#button");
+button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
+    console.log(email.value);
+});
+console.log(email);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -5,3 +5,22 @@ const person: person[] = ["ali", "faeze"];
 person.push("zahra")
 
 console.log(person)
+
+
+const email = document.querySelector<HTMLInputElement>("#email");
+document.addEventListener("click", () => {
+    console.log("ml")
+});
+
+email?.addEventListener("click", () => {
+    console.log("mjkk")
+});
+
+const button = document.querySelector<HTMLButtonElement>("#button");
+button?.addEventListener("click", () => {
+    console.log(email?.value)
+});
+
+
+
+console.log(email)
