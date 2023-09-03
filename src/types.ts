a7c8cd220f2f95c325130f673db0065fc56761d6
Faeze <faezeahmadi7173@gmail.com>
2023-09-03 03:39:19 +0330

    feat/faeze/js-10/src/types.ts

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type contactProps = {
+    id: string;
+    contactName: string;
+    contactNumber: string | Number;
+    storage: "SIM" | "Device";
+    avatar: string | null;
+
+}
+
+export type contactListType = contactProps[];
