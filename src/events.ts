e5fa7ff07f9707485b3cd46bf658e9547bcada6a
Faeze <faezeahmadi7173@gmail.com>
2023-09-03 11:49:30 +0330

    feat/faeze/js-11/remove-comments

--- src/events.ts
+++ src/events.ts
@@ -46,20 +46,7 @@ export const HandlerCreatContact = () => {
             contactName: newContact.contactName,
             contactNumber: newContact.contactNumber.toString(),
         });
-        
+
         listcontactas?.appendChild(listItem);
-        // const listItem = document.createElement("li");
-        // listItem.className = "py-4 px-2 bg-white rounded-lg";
-        // const contactNameItem = document.createElement("h2");
-        // contactNameItem.className = "text-slate-700";
-        // contactNameItem.innerText = newContact.contactName;
-        // const phoneNumberItem = document.createElement("p");
-        // phoneNumberItem.className = "text-slate-500";
-        // phoneNumberItem.innerText = newContact.contactNumber.toString();
-    
-    
-        // listItem.appendChild(contactNameItem);
-        // listItem.appendChild(phoneNumberItem);
-        // listcontactas?.appendChild(listItem);
     });   
 };
