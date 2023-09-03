b412679be5eed00005fdb743a20b3778d1cb7bc0
Faeze <faezeahmadi7173@gmail.com>
2023-09-03 03:55:25 +0330

    feat/faeze/js-10/src/importer.ts

---
+++ dist/src/importer.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.listcontactas = exports.removeButton = exports.drawer_List = exports.button_2 = exports.button = exports.Device = exports.SIM = exports.password = exports.email = void 0;
+exports.email = document.querySelector("#email");
+exports.password = document.querySelector("#password");
+exports.SIM = document.querySelector("#SIM");
+exports.Device = document.querySelector("#Device");
+exports.button = document.querySelector("#add_button");
+exports.button_2 = document.querySelector("#show_button");
+exports.drawer_List = document.querySelector("#drawer_List");
+exports.removeButton = document.querySelector("#removeButton");
+exports.listcontactas = document.querySelector("#listcontactas");
+//# sourceMappingURL=importer.js.map
