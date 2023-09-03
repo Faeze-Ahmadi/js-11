c35ffe60dc576951188e2329e1a58a74648a592c
Faeze <faezeahmadi7173@gmail.com>
2023-09-03 03:53:09 +0330

    feat/faeze/js-10/var-exports

--- dist/index.js
+++ dist/index.js
@@ -29,22 +29,21 @@ const listcontactas = document.querySelector("#listcontactas");
 // email?.addEventListener("click", () => {
 //     console.log("fm")
 // });
-button_2 === null || button_2 === void 0 ? void 0 : button_2.addEventListener("click", () => {
-    drawer_List === null || drawer_List === void 0 ? void 0 : drawer_List.classList.remove("bottom-[-100%]");
-    drawer_List === null || drawer_List === void 0 ? void 0 : drawer_List.classList.add("bottom-[-0%]");
+button_2?.addEventListener("click", () => {
+    drawer_List?.classList.remove("bottom-[-100%]");
+    drawer_List?.classList.add("bottom-[-0%]");
 });
-removeButton === null || removeButton === void 0 ? void 0 : removeButton.addEventListener("click", () => {
-    drawer_List === null || drawer_List === void 0 ? void 0 : drawer_List.classList.remove("bottom-[-0%]");
-    drawer_List === null || drawer_List === void 0 ? void 0 : drawer_List.classList.add("bottom-[-100%]");
+removeButton?.addEventListener("click", () => {
+    drawer_List?.classList.remove("bottom-[-0%]");
+    drawer_List?.classList.add("bottom-[-100%]");
 });
-button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
-    var _a, _b;
-    console.log(Device === null || Device === void 0 ? void 0 : Device.checked);
+button?.addEventListener("click", () => {
+    console.log(Device?.checked);
     const newContact = {
         id: crypto.randomUUID(),
-        contactName: (_a = email === null || email === void 0 ? void 0 : email.value) !== null && _a !== void 0 ? _a : '',
-        contactNumber: (_b = password === null || password === void 0 ? void 0 : password.value) !== null && _b !== void 0 ? _b : '',
-        storage: (Device === null || Device === void 0 ? void 0 : Device.checked) ? "Device" : "SIM",
+        contactName: email?.value ?? '',
+        contactNumber: password?.value ?? '',
+        storage: Device?.checked ? "Device" : "SIM",
         avatar: null,
     };
     contactList.push(newContact);
@@ -58,7 +57,7 @@ button === null || button === void 0 ? void 0 : button.addEventListener("click",
     phoneNumberItem.innerText = newContact.contactNumber.toString();
     listItem.appendChild(contactNameItem);
     listItem.appendChild(phoneNumberItem);
-    listcontactas === null || listcontactas === void 0 ? void 0 : listcontactas.appendChild(listItem);
+    listcontactas?.appendChild(listItem);
     console.log(contactList);
 });
 // console.log(email)
